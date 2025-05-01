5f0afab0815ad016af197543382fad06b159ed5e
Drackout <mikatx2@hotmail.com>
2025-05-01 17:37:41 +0100

    fix interfaces to e more friendly to newcomers

--- AnimalKingdom/ICanFly.cs
+++ AnimalKingdom/ICanFly.cs
@@ -7,6 +7,6 @@ namespace AnimalKingdom
 {
     public interface ICanFly
     {
-        int NumberOfWings {get; }
+        public int NumberOfWings {get; }
     }
 }
--- AnimalKingdom/IMammal.cs
+++ AnimalKingdom/IMammal.cs
@@ -7,6 +7,6 @@ namespace AnimalKingdom
 {
     public interface IMammal
     {
-        int NumberOfNipples {get;}
+        public int NumberOfNipples {get;}
     }
 }
