c825f8c46029adfeeff9d83713c09875aeba2454
Drackout <mikatx2@hotmail.com>
2025-05-01 19:21:29 +0100

    copy from pm1 to pm2

---
+++ PlayerManager2/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayerManager1 // >>> Change to PlayerManager2 for exercise 4 <<< //
+{
+    /// <summary>
+    /// The player listing program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The list of all players.
+        /// </summary>
+        private List<Player> playerList;
+
+        /// <summary>
+        /// Program begins here.
+        /// </summary>
+        private static void Main()
+        {
+            // Create a new instance of the player listing program
+            Program prog = new Program();
+            // Start the program instance
+            prog.Start();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the player listing program.
+        /// </summary>
+        private Program()
+        {
+            // Initialize the player list with two players using collection
+            // initialization syntax
+            playerList = new List<Player>() {
+                new Player("Best player ever", 100),
+                new Player("An even better player", 500)
+            };
+        }
+
+        /// <summary>
+        /// Start the player listing program instance
+        /// </summary>
+        private void Start()
+        {
+            // We keep the user's option here
+            string option;
+
+            // Main program loop
+            do
+            {
+                // Show menu and get user option
+                ShowMenu();
+                option = Console.ReadLine();
+
+                // Determine the option specified by the user and act on it
+                switch (option)
+                {
+                    case "1":
+                        InsertPlayer();
+                        break;
+                    case "2":
+                        ListPlayers(playerList);
+                        break;
+                    case "3":
+                        ListPlayersWithScoreGreaterThan();
+                        break;
+                    case "4":
+                        Console.WriteLine("Bye!");
+                        break;
+                    case "5":
+                        Console.Write("Show players with score higher than: ");
+                        int highS = int.Parse(Console.ReadLine());
+                        foreach (var pl in GetPlayersWithScoreGreaterThan(highS))
+                        {
+                            Console.WriteLine($"Name: {pl.Name}, Score: {pl.Score}");
+                        }
+                        break;
+                    default:
+                        Console.Error.WriteLine("\n>>> Unknown option! <<<\n");
+                        break;
+                }
+
+                // Wait for user to press a key...
+                Console.Write("\nPress any key to continue...");
+                Console.ReadKey(true);
+                Console.WriteLine("\n");
+
+                // Loop keeps going until players choses to quit (option 4)
+            } while (option != "4");
+        }
+
+        /// <summary>
+        /// Shows the main menu.
+        /// </summary>
+        private void ShowMenu()
+        {
+                Console.WriteLine("\n1 to add player");
+                Console.WriteLine("2 to list all players");
+                Console.WriteLine("3 to list players with score greater than");
+                Console.WriteLine("4 to Exit");
+                Console.WriteLine("5 GET players with score greater than");
+        }
+
+        /// <summary>
+        /// Inserts a new player in the player list.
+        /// </summary>
+        private void InsertPlayer()
+        {
+            Console.Write("Player Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Player Score: ");
+            string score = Console.ReadLine();
+            playerList.Add(new Player(name, int.Parse(score)));
+        }
+
+        /// <summary>
+        /// Show all players in a list of players. This method can be static
+        /// because it doesn't depend on anything associated with an instance
+        /// of the program. Namely, the list of players is given as a parameter
+        /// to this method.
+        /// </summary>
+        /// <param name="playersToList">
+        /// An enumerable object of players to show.
+        /// </param>
+        private static void ListPlayers(IEnumerable<Player> playersToList)
+        {
+            foreach (Player pl in playersToList)
+            {
+                Console.WriteLine($"Name: {pl.Name}, Score: {pl.Score}");
+            }
+        }
+
+        /// <summary>
+        /// Show all players with a score higher than a user-specified value.
+        /// </summary>
+        private void ListPlayersWithScoreGreaterThan()
+        {
+            Console.Write("Show players with score higher than: ");
+            int highS = int.Parse(Console.ReadLine());
+
+            foreach (Player pl in playerList)
+            {
+                if (pl.Score>highS)
+                    Console.WriteLine($"Name: {pl.Name}, Score: {pl.Score}");
+            }
+        }
+
+        /// <summary>
+        /// Get players with a score higher than a given value.
+        /// </summary>
+        /// <param name="minScore">Minimum score players should have.</param>
+        /// <returns>
+        /// An enumerable of players with a score higher than the given value.
+        /// </returns>
+        private IEnumerable<Player> GetPlayersWithScoreGreaterThan(int minScore)
+        {
+            foreach (Player item in playerList)
+            {      
+                if (item.Score > minScore)
+                    yield return item;   
+            }
+        }
+    }
+}
